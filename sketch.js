0b1862192b76c6d7d2354463ccbaca1be9d1590d
siya579 <79684371+siya579@users.noreply.github.com>
2021-04-07 15:09:50 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,61 +1,132 @@
-var canvas,bg;
-var together;
-var tom, tomImg1,tomImg2;
-var jerry, jerryImg1,jerryImg2;
 
-function preload() {
-    bg = loadImage("images/garden.png");
-    tomImg1= loadAnimation("images/tomOne.png");
-    tomImg2=loadAnimation("images/tomTwo.png","images/tomThree.png");
-    tomImg3= loadAnimation("images/tomFour.png");
-    jerryImg1=loadAnimation("images/jerryOne.png");
-    jerryImg2= loadAnimation("images/jerryTwo.png","images/jerryThree.png");
-    jerryImg3=loadAnimation("images/jerryFour.png");
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Render = Matter.Render;
+const Constraint=Matter.Constraint;
+var treeObj, stoneObj,groundObject, launcherObject;
+var mango1,mango2,mango3,mango4,mango5,mango6,mango7,mango8,mango9,mango10,mango11,mango12;
+var world,boy;
+var launchingForce=100;
 
-}
+function preload(){
+	boy=loadImage("images/boy.png");
+  }
 
-function setup(){
-    canvas = createCanvas(1000,800);
+function setup() {
+	createCanvas(1300, 600);
+	engine = Engine.create();
+	world = engine.world;
 
-    tom = createSprite(870, 600);
-    tom.addAnimation("tomSleeping", tomImg1);
-    tom.scale = 0.2;
+	stoneObj=new stone(235,420,30); 
 
-    jerry = createSprite(200, 600);
-    jerry.addAnimation("jerryStanding", jerryImg1);
-    jerry.scale = 0.15;
+	mango1=new mango(1100,100,30);
+  mango2=new mango(1170,130,30);
+	mango3=new mango(1010,140,30);
+	mango4=new mango(1000,70,30);
+	mango5=new mango(1100,70,30);
+	mango6=new mango(1000,230,30);
+	mango7=new mango(900,230,25);
+	mango8=new mango(1140,150,25);
+	mango9=new mango(1100,230,25);
+	mango10=new mango(1200,200,25);
+	mango11=new mango(1120,50,25);
+	mango12=new mango(900,160,25);
 
+	treeObj=new tree(1050,580);
+	groundObject=new ground(width/2,600,width,20);
+	launcherObject=new launcher(stoneObj.body,{x:235,y:420})
+  // var render = Render.create({
+  //   element: document.body,
+  //   engine: engine,
+  //   options: {
+  //     width: 1300,
+  //     height: 600,
+  //     wireframes: false
+  //   }
+  // });
+	
+	Engine.run(engine);
+ // Render.run(render);
 }
 
 function draw() {
 
-    background(bg);
+  background(230);
+  //frameRate(2)
+ // Engine.update(engine)
+  textSize(25);
+  text("Press Space to get a second Chance to Play!!",50 ,50);
+  image(boy ,200,340,200,300);
+  //Engine.update(engine)
+  
 
-    if(tom.x - jerry.x < (tom.width - jerry.width)/2)
-    { 
-        tom.velocityX=0;
-        tom.addAnimation("tomLastImage", tomImg3);
-        tom.x =300;
-        tom.scale=0.2;
-        tom.changeAnimation("tomLastImage");
-        jerry.addAnimation("jerryLastImage", jerryImg3);
-        jerry.scale=0.15;
-        jerry.changeAnimation("jerryLastImage");
-    }  
+  treeObj.display();
+  stoneObj.display();
+  mango1.display();
+  mango2.display();
+  mango3.display();
+  mango4.display();
+  mango6.display();
+ mango7.display();
+  mango8.display();
+  mango9.display();
+  mango10.display();
+  mango11.display();
+  mango12.display();
+  stoneObj.display();
 
-    drawSprites();
+  groundObject.display();
+  launcherObject.display();
+  detectollision(stoneObj,mango1);
+  detectollision(stoneObj,mango2);
+  detectollision(stoneObj,mango3);
+  detectollision(stoneObj,mango4);
+  detectollision(stoneObj,mango5);
+  detectollision(stoneObj,mango6);
+  detectollision(stoneObj,mango7);
+  detectollision(stoneObj,mango8);
+  detectollision(stoneObj,mango9);
+  detectollision(stoneObj,mango10);
+  detectollision(stoneObj,mango11);
+  detectollision(stoneObj,mango12);
 }
 
+function mouseDragged()
+{
+	Matter.Body.setPosition(stoneObj.body, {x:mouseX, y:mouseY}) 
+}
 
-function keyPressed(){
+function mouseReleased()
+{
+	launcherObject.fly();
+    // distance=int(dist(stoneObj.x,stoneObj.y,mango1.x,mango1.y));
+}
 
-    if(keyCode === LEFT_ARROW){
-        tom.velocityX = -5; 
-        tom.addAnimation("tomRunning", tomImg2);
-        tom.changeAnimation("tomRunning");
-        
-        jerry.addAnimation("jerryTeasing", jerryImg2);
-        jerry.frameDelay = 25;
-        jerry.changeAnimation("jerryTeasing");
+function keyPressed() {
+	if (keyCode === 32) {
+    Matter.Body.setPosition(stoneObj.body, {x:235, y:420}) 
+	  launcherObject.attach(stoneObj.body);
+	}
+  }
+
+  function detectollision(lstone,lmango){
+	/*var collision = Matter.SAT.collides(lstone,lmango);
+	if(collision.collided){
+		console.log("collided");
+		Matter.Body.setStatic(lmango,false);	
+	}*/
+  mangoBodyPosition=lmango.body.position
+  stoneBodyPosition=lstone.body.position
+  
+  var distance=dist(stoneBodyPosition.x, stoneBodyPosition.y, mangoBodyPosition.x, mangoBodyPosition.y)
+  //console.log(distance)
+ // console.log(lmango.r+lstone.r)
+  	if(distance<=lmango.r+lstone.r)
+    {
+      //console.log(distance);
+  	  Matter.Body.setStatic(lmango.body,false);
     }
-}
+
+  }
